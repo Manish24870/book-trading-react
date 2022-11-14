47e482dde19b291a6b2299f5494f0c9c7dfb703e
Manish Acharya <manish24870@gmail.com>
2022-11-14 21:43:22 +0545

    mantine setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import { MantineProvider, ColorSchemeProvider } from "@mantine/core";
+import { NotificationsProvider } from "@mantine/notifications";
 
 import store from "./app/store";
+import theme from "./utils/mantine/theme";
 
 const App = () => {
   return (
     <Provider store={store}>
-      <p>Book Trading App</p>
+      <BrowserRouter>
+        <MantineProvider withGlobalStyles withNormalizeCSS theme={theme}>
+          <NotificationsProvider>
+            <p>Book Trading App</p>
+          </NotificationsProvider>
+        </MantineProvider>
+      </BrowserRouter>
     </Provider>
   );
 };
---
+++ src/utils/mantine/theme.js
@@ -0,0 +1,27 @@
+const theme = {
+  fontFamily: "'Poppins', sans-serif",
+  headings: {
+    fontFamily: "'Poppins', sans-serif",
+  },
+  colors: {
+    primary: [
+      "#ECF0F8",
+      "#CBD6EC",
+      "#A9BBDF",
+      "#88A1D3",
+      "#6687C7",
+      "#456CBA",
+      "#375795",
+      "#294170",
+      "#1B2B4B",
+      "#0E1625",
+    ],
+  },
+  primaryColor: "primary",
+  // primaryShade: {
+  //   light: 5,
+  //   dark: 5,
+  // },
+};
+
+export default theme;
